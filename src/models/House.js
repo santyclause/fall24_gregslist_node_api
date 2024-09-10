48d24388f02bfdfa246a1ca751b7c4367ad7a7aa
Matthew Marchal <matthew.marchal@gmail.com>
2024-09-10 14:10:11 -0600

    created house schema

---
+++ src/models/House.js
@@ -0,0 +1,25 @@
+import { Schema } from "mongoose";
+
+
+export const HouseSchema = new Schema(
+  {
+    bedrooms: { type: Number, min: 0, max: 30, required: true },
+    bathrooms: { type: Number, min: 0, max: 25, required: true },
+    levels: { type: Number, min: 1, max: 4, required: true },
+    price: { type: Number, min: 0, max: 10000000, required: true },
+    imgUrl: { type: String, minlength: 0, maxlength: 500, required: true },
+    description: { type: String, minlength: 0, maxlength: 500 },
+    year: { type: Number, min: 1000, max: 2024 },
+    creatorId: { type: Schema.ObjectId, required: true, ref: 'Account' }
+  },
+  {
+    timestamps: true, toJSON: { virtuals: true }
+  }
+)
+
+HouseSchema.virtual('creator', {
+  localField: 'creatorId',
+  foreignField: 'creatorId',
+  ref: 'Account',
+  justOne: true
+})
